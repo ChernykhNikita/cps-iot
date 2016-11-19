98b2c6345eb9a9ef25799d08e3631dd6e65ddf25
root <chernyh.studbox@gmail.com>
2016-11-19 16:54:36 +0700

    Saturday 19th examples

--- 2diod.js
+++ 2diod.js
@@ -5,6 +5,8 @@ console.log("Starting the code");
 
 var board = new firmata.Board("/dev/ttyACM0", function(){
     console.log("Connecting to Arduino");
+    console.log("Activation of pin 8");
+    board.pinMode(8, board.MODES.OUTPUT);
     console.log("Activation of pin 13");
     board.pinMode(13, board.MODES.OUTPUT);      // pin13 as out
 });
@@ -22,6 +24,14 @@ http.createServer(function(req,res){
         board.digitalWrite(13, board.HIGH);
     }
     
+    if (operator == 2) {
+        console.log("Putting LED to OFF.");
+        board.digitalWrite(8, board.LOW);
+    }
+    if (operator == 3) {
+        console.log("Putting LED to ON.");
+        board.digitalWrite(8, board.HIGH);
+    }
     
     res.writeHead(200, {"Content-Type":"text/plain"}); //200=OK
     res.write("For test write into browser e.g. 123.1.2.3:8080/1 \n");
