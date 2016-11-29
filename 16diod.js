b3e3d81c5fa55988d433abdec63a0bc92a8968d9
root <chernyh.studbox@gmail.com>
2016-11-29 11:07:49 +0700

    29.11

---
+++ 16diod.js
@@ -0,0 +1,135 @@
+var http = require("http").createServer(handler); // on req - hand
+var io = require("socket.io").listen(http);         //socket library
+var fs = require("fs");                             //variable for file system for providing html
+var firmata = require("firmata");
+
+console.log("Starting the code");
+
+var board = new firmata.Board("/dev/ttyACM0", function(){
+    console.log("Connecting to Arduino");
+    board.pinMode(0, board.MODES.ANALOG); // enable analog pin 0
+    board.pinMode(1, board.MODES.ANALOG); // enable analog pin 1
+    board.pinMode(2, board.MODES.OUTPUT); // direction of DC motor
+    board.pinMode(3, board.MODES.PWM); // PWM of motor i.e. speed of rotation
+    board.pinMode(4, board.MODES.OUTPUT); // direction DC motor
+});
+
+function handler(req,res){
+   fs.readFile(__dirname + "/16diod.html",
+   function (err, data) {
+    if (err) {
+        res.writeHead(500, {"Content-Type":"text/plain"});
+        return res.end("Error loading html page.");
+        }
+    
+    res.writeHead(200);
+    res.end(data);
+    })
+
+}
+
+var desiredValue = 0; // desired value var
+var actualValue = 0; // actual value var
+
+var factor = 0.1; // proportional factor that determines the speed of aproaching toward desired value
+
+var Kp = 0.55; // proportional factor
+var Ki = 0.008; // integral factor
+var Kd = 0.15; // differential factor
+var pwm = 0;
+var pwmLimit = 254;
+
+var err = 0; // variable for second pid implementation
+var errSum = 0; // sum of errors
+var dErr = 0; // difference of error
+var lastErr = 0; // to keep the value of previous error to estimate derivative
+
+var controlAlgorihtmStartedFlag = 1; // set flag that the algorithm has started
+var intervalCtrl; // var for setInterval in global space
+
+var sendValueViaSocket = function() {}; // function to send message over socket
+var sendStaticMsgViaSocket = function() {}; // function to send static message over socket
+
+http.listen(8080); // server will listen on port 8080
+
+var sendValueViaSocket = function(){}; // var for sending messages
+
+board.on("ready", function(){
+    
+board.analogRead(0, function(value){
+   desiredValue = value; // continous read of analog pin 0 
+});
+
+board.analogRead(1, function(value){
+   actualValue = value; // continous read of analog pin 1
+});
+
+
+io.sockets.on("connection", function(socket) {
+    socket.emit("messageToClient", "Srv connected, brd OK");
+    socket.emit("staticMsgToClient", "Server connected, board ready.")
+    
+    setInterval(sendValues, 40, socket); // on 40ms trigger func. sendValues
+    
+    socket.on("startControlAlgorithm", function(numberOfControlAlgorithm){
+        startControlAlgorithm(numberOfControlAlgorithm);
+    });
+    
+    socket.on("stopControlAlgorithm", function(){
+        stopControlAlgorithm();
+    });
+    
+    
+    sendValueViaSocket = function (value) {
+        io.sockets.emit("messageToClient", value);
+    }
+    
+    sendStaticMsgViaSocket = function (value) {
+        io.sockets.emit("staticMsgToClient", value);
+    }
+    
+    
+}); // end of sockets.on connection
+
+}); // end of board.on ready
+
+function controlAlgorithm () {
+    err = desiredValue - actualValue; // error as difference between desired and actual var.
+    errSum += err; // sum of errors | like integral
+    dErr = err - lastErr; // difference of error
+    pwm = Kp*err + Ki*errSum + Kd*dErr;
+    lastErr = err; // save the value for the next cycle
+    if(pwm > pwmLimit) {pwm = pwmLimit}; // to limit the value for pwm / positive
+    if(pwm < -pwmLimit) {pwm = -pwmLimit}; // to limit the value for pwm / negative
+    if (pwm > 0) {board.digitalWrite(2,1); board.digitalWrite(4,0);}; // direction if > 0
+    if (pwm < 0) {board.digitalWrite(2,0); board.digitalWrite(4,1);}; // direction if < 0
+    board.analogWrite(3, Math.abs(pwm));
+};
+
+
+function startControlAlgorithm () {
+    if (controlAlgorihtmStartedFlag == 0) {
+        controlAlgorihtmStartedFlag = 1; // set flag that the algorithm has started
+        intervalCtrl = setInterval(function() {controlAlgorithm(); }, 30); // na 30ms klic
+        console.log("Control algorithm started");
+    }
+};
+
+function stopControlAlgorithm () {
+    clearInterval(intervalCtrl); // clear the interval of control algorihtm
+    board.analogWrite(3,0); // write 0 on pwm pin to stop the motor
+    controlAlgorihtmStartedFlag = 0; // set flag that the algorithm has stopped
+};
+
+function sendValues (socket) {
+    socket.emit("clientReadValues",
+    { // json notation between curly braces
+    "desiredValue": desiredValue,
+    "actualValue": actualValue,
+    "pwm": pwm
+    });
+    console.log(desiredValue);
+};
+    
+
+
